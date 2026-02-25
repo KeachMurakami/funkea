076529d91e363fc14a922090827e34a3b6874bc8
keach_murakami <keach_murakami@2025-40-00005.local>
2026-02-25 11:02:53 +0900

    bibtex is now managed at ~/Dropbox/Pulish

--- R/bibtex.R
+++ R/bibtex.R
@@ -1,20 +1,67 @@
 #' @export
 #' @importFrom rlang .data
 update_bib <-
-  function(bib_directory = "~/Dropbox/r_packages/fudukue/bibtex/list_bib/",
-           output_dir = "~/Dropbox/r_packages/fudukue/bibtex/"){
-    if(length(dir(bib_directory)) == 0){
+  function(
+    bib_directory = "~/Dropbox/Publish/bibtex/list_bib/",
+    output_dir = "~/Dropbox/Publish/bibtex/"
+  ) {
+    if (length(dir(bib_directory)) == 0) {
       stop("Folder may not exist.")
     } else {
-      system(paste0("cat ", bib_directory, "*.bib > ", output_dir, "full_with.bib"))
-      system(paste0("cat ", output_dir, "*with.bib > ", output_dir, "With.bib"))
+      tmp <- tempdir()
 
-      system(paste0("mv ", output_dir, "full_with.bib ", output_dir, "full_without.bib"))
-      system(paste0("cat ", output_dir, "*without.bib > ", output_dir, "Without.bib"))
+      # Step 1: list_bib/ の全bibを結合
+      system(paste0("cat ", bib_directory, "*.bib > ", tmp, "/full_base.bib"))
 
-      system(paste0("mv ", output_dir, "full_without.bib ", output_dir, "tempFull.bib"))
-      system(paste0("cat ", output_dir, "*Full.bib > ", output_dir, "Full.bib"))
-      system(paste0("rm ", output_dir, "tempFull.bib"))
+      # Step 2: With.bib = full_base + abbrev_with
+      system(paste0(
+        "cat ",
+        tmp,
+        "/full_base.bib ",
+        output_dir,
+        "abbrev_with.bib > ",
+        tmp,
+        "/With.bib"
+      ))
+
+      # Step 3: Without.bib = full_base + abbrev_without
+      system(paste0(
+        "cat ",
+        tmp,
+        "/full_base.bib ",
+        output_dir,
+        "abbrev_without.bib > ",
+        tmp,
+        "/Without.bib"
+      ))
+
+      # Step 4: Full.bib = full_base + abbrev_Full
+      system(paste0(
+        "cat ",
+        tmp,
+        "/full_base.bib ",
+        output_dir,
+        "abbrev_Full.bib > ",
+        tmp,
+        "/Full.bib"
+      ))
+
+      # 一括コピー
+      file.copy(
+        file.path(tmp, "With.bib"),
+        file.path(output_dir, "With.bib"),
+        overwrite = TRUE
+      )
+      file.copy(
+        file.path(tmp, "Without.bib"),
+        file.path(output_dir, "Without.bib"),
+        overwrite = TRUE
+      )
+      file.copy(
+        file.path(tmp, "Full.bib"),
+        file.path(output_dir, "Full.bib"),
+        overwrite = TRUE
+      )
     }
     cat("\nCitations have been updated.\n\n")
   }
@@ -22,16 +69,30 @@ update_bib <-
 #' @export
 #' @importFrom rlang .data
 update_abbrv <-
-  function(abbrv_file = "~/Dropbox/r_packages/fudukue/bibtex/MyAbbrvList.csv"){
+  function(abbrv_file = "~/Dropbox/Publish/bibtex/MyAbbrvList.csv") {
     abbrv_dir <- dirname(abbrv_file)
     abbrv_data <-
-      dplyr::mutate(utils::read.csv(abbrv_file),
-                    Full = paste0('@string{', .data$Abbrv, '=\"', .data$Full, '\"}'),
-                    With = paste0('@string{', .data$Abbrv, '=\"', .data$Abbrv_with_period, '\"}'),
-                    Without = paste0('@string{', .data$Abbrv, '=\"', .data$Abbrv_without_period, '\"}'))
+      dplyr::mutate(
+        utils::read.csv(abbrv_file),
+        Full = paste0('@string{', .data$Abbrv, '=\"', .data$Full, '\"}'),
+        With = paste0(
+          '@string{',
+          .data$Abbrv,
+          '=\"',
+          .data$Abbrv_with_period,
+          '\"}'
+        ),
+        Without = paste0(
+          '@string{',
+          .data$Abbrv,
+          '=\"',
+          .data$Abbrv_without_period,
+          '\"}'
+        )
+      )
 
-    write(abbrv_data$Full,    file = paste0(abbrv_dir, "/abbrev_Full.bib"))
-    write(abbrv_data$With,    file = paste0(abbrv_dir, "/abbrev_with.bib"))
+    write(abbrv_data$Full, file = paste0(abbrv_dir, "/abbrev_Full.bib"))
+    write(abbrv_data$With, file = paste0(abbrv_dir, "/abbrev_with.bib"))
     write(abbrv_data$Without, file = paste0(abbrv_dir, "/abbrev_without.bib"))
 
     cat("\nAbbreviations have been updated.\n\n")
@@ -40,7 +101,7 @@ update_abbrv <-
 
 #' @export
 update_cite <-
-  function(...){
+  function(...) {
     update_abbrv(...)
     update_bib(...)
   }
